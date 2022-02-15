1a6b4277c286d28828a4411c790cc6b5fe58a6cc
Polina Uzhvak <uzhvak@istories.media>
2022-02-15 10:30:12 +0300

    Delete input, change output

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 import datetime
-name = input("What's your name? ")
-print("Hello, ", name,"!")
-print("We learn Git")
-print("We learn GitHub")
+print("Hello!")
+print("We learn Git and GitHub")
 print("Our project: Git_Tutorial")
 print("Today: ", datetime.today().strftime('%d/%m/%Y'))

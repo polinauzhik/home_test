2732c852ceaae2d396c8d07d91e0ae68e831839b
Polina Uzhvak <uzhvak@istories.media>
2022-02-15 09:44:21 +0300

    Start 4 lesson

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-from datetime import datetime
-print("Hello!")
-print("We learn git")
+name = input("What's your name? ")
+print("Hello, ", name,"!")
+print("We learn Git")
 print("We learn GitHub")
-print("Our project: Git_Tutorial")
-print("Today: ", datetime.today().strftime('%d/%m/%Y'))
-1+1
-2+2
-3+3
